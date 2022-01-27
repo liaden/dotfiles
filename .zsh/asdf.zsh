b74f8461110c25ff652f5997461656b1c8638a1d
Joel Johnson <johnson.joel.b@gmail.com>
2022-01-26 20:13:21 -0500

    Fix for updated asdf version

--- .zsh/asdf.zsh
+++ .zsh/asdf.zsh
@@ -1,7 +1,7 @@
 if [[ -d /usr/local/opt/asdf ]]; then
-    export ASDF_DIR="/usr/local/opt/asdf"
+    export ASDF_DIR="/usr/local/opt/asdf/libexec"
     source $ASDF_DIR/asdf.sh
 elif [[ -d /home/linuxbrew/.linuxbrew/opt/asdf ]]; then
-    export ASDF_DIR="/home/linuxbrew/.linuxbrew/opt/asdf"
+    export ASDF_DIR="/home/linuxbrew/.linuxbrew/opt/asdf/libexec"
     source $ASDF_DIR/asdf.sh
 fi
